0441f10ec001ef4753a32284e12e9709c8c59b2e
cyberzek-max <c1zek1011@gmail.com>
2026-01-13 14:41:14 +0400

    web

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$#" -ne 3 ] && { echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"; exit 1; }; curl -i -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
+curl -i -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
