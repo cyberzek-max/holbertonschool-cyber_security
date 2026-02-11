e0a5b901d2effd7bf90547eb11d2a69a12e9bc89
cyberzek-max <c1zek1011@gmail.com>
2026-02-11 15:43:10 +0400

    protocols

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 whois "$1" 2>/dev/null | awk 'BEGIN{FS=":[ \t]*"} $1~/^Registrant /{s="Registrant"} $1~/^Admin /{s="Admin"} $1~/^Tech /{s="Tech"} s && $1~/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext|Fax|Fax Ext|Email)$/{f=$1; sub(/^(Registrant|Admin|Tech)[ ]+/,"",f); if(f=="Street") printf "%s %s, %s ",s,f,$2; else if(f~/Phone Ext|Fax Ext/) printf "%s %s:, %s",s,f,$2; else printf "%s %s, %s",s,f,$2; c++; if(c<36) printf "\n"}' > "$1.csv"
-
