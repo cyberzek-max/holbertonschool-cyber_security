a063999d905ee6b97a2af5173c54c986d0b8de08
cyberzek-max <c1zek1011@gmail.com>
2026-02-11 14:46:44 +0400

    protocols

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E '^(PermitRootLogin|PasswordAuthentication|X11Forwarding|PermitEmptyPasswords|Protocol|MaxAuthTries|AllowTcpForwarding)' /etc/ssh/sshd_config 2>/dev/null | grep -vE '^#|yes$|no$|sandbox$' | sed 's/^/[!] Non-standard or insecure: /'
+grep -E '^(PermitRootLogin|PasswordAuthentication|X11Forwarding|PermitEmptyPasswords|Protocol|MaxAuthTries|AllowTcpForwarding|PermitUserEnvironment|GSSAPIAuthentication|KerberosAuthentication)' /etc/ssh/sshd_config 2>/dev/null | grep -vE '^#|yes$|no$|sandbox$|prohibit-password$' | sed 's/^/[!] Non-standard or insecure: /'
